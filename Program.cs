79a5b1559fabb9c4367a835aeecc83cc55d2ebb2
Melissa <hughes.melissa1@gmail.com>
2016-09-09 14:49:55 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortune_Teller_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       {
+                Console.WriteLine("What is your FIRST NAME?");
+                string firstName = (Console.ReadLine());
+
+            }
+            Console.WriteLine("What is your LAST NAME?");
+
+            Console.WriteLine("What is your AGE NAME?");
+
+            Console.WriteLine("What is your birth month?");
+
+            Console.WriteLine("What is your favorite ROYGBIV color?\n*Don't know what \"ROYGBIV\" is? Just type\"HELP\" for a list.*");
+
+            Console.WriteLine("How many siblings do you have?");
+
+
+
+
+
+        }
+    }
+}
